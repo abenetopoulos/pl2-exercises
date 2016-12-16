ba8feb025807f95acdc13f96385feb5f00362056
abenetopoulos <abenetopoulos@users.noreply.github.com>
2016-12-16 14:13:08 +0200

    seems alright apart from the fact taht we can't ensure the result if best == N

--- ex3/count_same.c
+++ ex3/count_same.c
@@ -2,41 +2,54 @@
   @     \forall integer i; left <= i < right ==> \at(x[i], L) == \at(x[left], L);
 */
 
-/*@ predicate is_max_subseq{L}(int *x, integer kCandidate, integer left, integer right, integer best) =
-  @     \forall integer i; left <= i < right && i != kCandidate ==> !valid_subseq(x, i, i + best);
-*/
-
-/*@ predicate some_pred{L}(int *x, integer k, integer best) = 
-  @     \at(x[k + best + 1],L) == \at(x[k], L) ? valid_subseq(x, k, k + best) : 0 > 1;
-*/
-
 /*@ requires 1 <= N <= 1000000;
   @ requires \valid(x + (0..N-1));
+  @ ensures 1 <= \result <= N;
   @ ensures \exists integer k; 0 <= k < N && valid_subseq(x, k, k + \result);
+  @ ensures \result < N ==> \exists integer k; 0 <= k < N && valid_subseq(x, k, k + \result) ==> x[k] != x[k + \result];
+  @ ensures \result < N ==> (\exists integer k; 0 <= k < N && valid_subseq(x, k, k + \result) ==> 
+            \forall integer l; 0 <= l < N && l != k ==> !valid_subseq(x, l, l + \result + 1));
 */
 int countSameConsecutive(int N, int x[]) {
     int best = 0, i = 0;
+
     /*@ loop invariant 0 <= i <= N;
       @ loop invariant i == 0 ==> best == 0;
-      @ loop invariant i > 0 ==> 1 <= best <= i;
+      @ loop invariant i > 0 ==> 1 <= best <= N;
       @ loop invariant i > 0 ==> \exists integer k; 0 <= k < i && valid_subseq(x, k, k + best);
+      @ loop invariant i > 0 ==> (\exists integer k; 0 <= k < i && valid_subseq(x, k, k + best) ==> x[k] != x[k + best]);
+      @ loop invariant i > 0 ==> (\exists integer k; 0 <= k < i && valid_subseq(x, k, k + best) ==> 
+            \forall integer l; 0 <= l < i && l != k ==> !valid_subseq(x, l, l + best + 1));
       @ loop assigns i, best;
       @ loop variant N - i;
     */
     while (i < N) {
         int j = i+1;
         /*@ loop invariant i < j <= N;
-          @ loop invariant \forall integer k; i <= k < j ==> x[k] == x[i];
+          @ loop invariant valid_subseq(x, i, j);
           @ loop assigns j;
           @ loop variant N - j;
         */
         while (j < N && x[j] == x[i]) ++j;
-        /*@ assert j < N ==> x[j] != x[i];
-         */
+        /*@ assert (j < N ==> (x[j] != x[i] && x[j] != x[j - 1])) || j == N;
+        */
+        /*@ assert i == 0 && j == N ==> valid_subseq(x, 0, N);*/
         if (j-i > best) best = j-i;
         /*@ assert best >= j - i;
         */
         i = j;
     }
+
+    /*@ assert best <= N;*/
     return best;
 }
+
+#if 0
+  @ ensures \result == N ==> valid_subseq(x, 0, N);
+  @ ensures \exists integer k; 0 <= k < N && valid_subseq(x, k, k + \result);
+  @ loop invariant \forall integer k; i <= k < j ==> x[k] == x[i];
+  @ loop invariant 0 <= best <= N;
+  @ loop invariant i == 0 ==> best == 0;
+  @ ensures \exists integer k; 0 <= k < N && valid_subseq(x, k, k + \result) ==> 
+            \forall integer l; 0 <= l < N && l != k ==> !valid_subseq(x, l, l + \result + 1);
+#endif
